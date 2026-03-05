7f2b6155e877cc74d151a1241bf0be59011086db
Helen <helenjling@gmail.com>
2026-03-04 19:43:34 -0700

    Changed the 2 different launch positions to be on different commands along with their respective hood positions

--- src/main/java/frc/robot/commands/LaunchSetpoint1.java
+++ src/main/java/frc/robot/commands/LaunchSetpoint1.java
@@ -4,18 +4,18 @@
 
 package frc.robot.commands;
 
-import static frc.robot.Constants.ShooterConstants.SETPOINT_2_RPM;
+import static frc.robot.Constants.ShooterConstants.SETPOINT_1_RPM;
 
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.shooter.Shooter;
 
-public class Launch extends Command {
+public class LaunchSetpoint1 extends Command {
   private final Shooter m_shooter;
   private final CommandXboxController m_controller;
 
-  public Launch(Shooter shooter, CommandXboxController controller) {
+  public LaunchSetpoint1(Shooter shooter, CommandXboxController controller) {
     m_shooter = shooter;
     m_controller = controller;
 
@@ -24,13 +24,13 @@ public class Launch extends Command {
 
   @Override
   public void initialize() {
-    m_shooter.setVelocity(SETPOINT_2_RPM);
+    m_shooter.setVelocity(SETPOINT_1_RPM);
   }
 
   @Override
   public void execute() {
-    m_shooter.setVelocity(SETPOINT_2_RPM);
-    if (m_shooter.getVelocity() > SETPOINT_2_RPM) {
+    m_shooter.setVelocity(SETPOINT_1_RPM);
+    if (m_shooter.getVelocity() > SETPOINT_1_RPM) {
       m_controller.getHID().setRumble(RumbleType.kBothRumble, 1.0);
     } else {
       m_controller.getHID().setRumble(RumbleType.kBothRumble, 0.0);
