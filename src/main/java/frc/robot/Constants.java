6d98a6615c6561a98dc21411dfa53164fdd604d4
Helen <helenjling@gmail.com>
2026-03-19 19:49:02 -0700

    estimated camera translations

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -266,16 +266,16 @@ public final class Constants {
     // (Not used by Limelight, configure in web UI instead)
     public static Transform3d robotToCamera0 =
         new Transform3d(
-            Units.inchesToMeters(11.0),
-            Units.inchesToMeters(11.0),
+            Units.inchesToMeters(-13.0),
+            Units.inchesToMeters(13.0),
             Units.inchesToMeters(6.0),
-            new Rotation3d(0.0, Units.degreesToRadians(90), Units.degreesToRadians(90)));
+            new Rotation3d(0.0, Units.degreesToRadians(45), Units.degreesToRadians(225)));
     public static Transform3d robotToCamera1 =
         new Transform3d(
-            Units.inchesToMeters(11.0),
-            -Units.inchesToMeters(11.0),
+            Units.inchesToMeters(-13.0),
+            -Units.inchesToMeters(-13.0),
             Units.inchesToMeters(6.0),
-            new Rotation3d(0.0, Units.degreesToRadians(90), -Units.degreesToRadians(135)));
+            new Rotation3d(0.0, Units.degreesToRadians(45), -Units.degreesToRadians(315)));
 
     // Basic filtering thresholds
     public static double maxAmbiguity = 0.3;
