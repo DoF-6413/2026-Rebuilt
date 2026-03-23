1a334fd0367591685ba6c6cbbbb1efbaad34c482
Lohit Paruchuri <lohit.paruchuri@gmail.com>
2026-03-22 21:37:31 -0700

    estimated the camera translations from the gyro

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -265,17 +265,17 @@ public final class Constants {
     // Robot to camera transforms
     // (Not used by Limelight, configure in web UI instead)
     public static Transform3d robotToCamera0 =
-        new Transform3d(
-            Units.inchesToMeters(11.0),
-            Units.inchesToMeters(11.0),
-            Units.inchesToMeters(6.0),
-            new Rotation3d(0.0, Units.degreesToRadians(90), Units.degreesToRadians(90)));
+      new Transform3d(
+        Units.inchesToMeters(-13.0),
+        Units.inchesToMeters(13.0),
+        Units.inchesToMeters(6.0),
+        new Rotation3d(0.0, Units.degreesToRadians(45), Units.degreesToRadians(225)));
     public static Transform3d robotToCamera1 =
-        new Transform3d(
-            Units.inchesToMeters(11.0),
-            -Units.inchesToMeters(11.0),
-            Units.inchesToMeters(6.0),
-            new Rotation3d(0.0, Units.degreesToRadians(90), -Units.degreesToRadians(135)));
+      new Transform3d(
+        Units.inchesToMeters(-13.0),
+        -Units.inchesToMeters(-13.0),
+        Units.inchesToMeters(6.0),
+        new Rotation3d(0.0, Units.degreesToRadians(45), -Units.degreesToRadians(315)));
 
     // Basic filtering thresholds
     public static double maxAmbiguity = 0.3;
